b8b09218264a2f95f86c47a308547583f5afd024
khh-ini <kiki.h.hutapea@gmail.com>
2022-03-25 12:36:45 +0700

    recipe data model

--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
-func IndexHandler(c *gin.Context) {
-	name := c.Params.ByName("name")
-	c.JSON(200, gin.H{
-		"ping": "pong " + name,
-	})
+type Recipe struct {
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"publishedAt"`
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/:name", IndexHandler)
 	router.Run()
 }
